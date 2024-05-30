6fa6d710af4bf59bd47a9c122c78a789e30e28f8
cs-amol-phatangade <aphatangade@fortinet.com>
2024-05-30 18:58:24 +0530

    Adding connector actions for Application Management

--- eset-protect-enterprise/operations.py
+++ eset-protect-enterprise/operations.py
@@ -5,124 +5,78 @@ logger = get_logger('eset-protect-enterprise')
 
 
 def authenticate(config):
-    username = config.get("server_username")
-    password = config.get("server_password")
-    base_url = config.get("base_url")
-    auth_url = f"{base_url}/api/authentication"
-    auth_data = {"username": username, "password": password}
-    response = requests.post(auth_url, json=auth_data)
-    if response.status_code == 200:
-        return response.json()["token"]
-    else:
-        print("Authentication failed.")
-        return None
-
-
-def list_endpoints(config, params):
     try:
+        username = config.get("server_username")
+        password = config.get("server_password")
         base_url = config.get("base_url")
-        token = authenticate()
-        endpoints_url = f"{base_url}/api/endpoints"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(endpoints_url, headers=headers)
+        auth_url = f"{base_url}/oauth/token"
+        auth_data = {"grant_type":"password", "username": username, "password": password, "refresh_token": "refresh_token"}
+        headers = {"accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.post(auth_url, data=auth_data, headers=headers)
         if response.status_code == 200:
-            return response.json()["endpoints"]
+            return response.json()["access_token"]
         else:
-            logger.error("Failed to list endpoints.")
+            logger.error("Authentication failed.")
+            raise ConnectorError('Authentication failed. {}'.format(str(response)))
     except Exception as err:
         logger.exception('An exception occurred {}'.format(str(err)))
         raise ConnectorError('An exception occurred {}'.format(str(err)))
 
 
-def isolate_endpoint(config, params):
+def get_executables(config, params):
     try:
-        base_url = config.get("base_url")
-        endpoint_id = params.get("endpoint_id")
-        token = authenticate()
-        isolate_url = f"{base_url}/api/endpoints/{endpoint_id}/isolate"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post(isolate_url, headers=headers)
-        if response.status_code == 200:
-            logger.info(f"Endpoint {endpoint_id} has been isolated successfully.")
-            return True
+        token = authenticate(config)
+        base_url = "https://eu.application-management.eset.systems" #config.get("base_url")
+        headers = {"Authorization": token, "accept": "application/json"}
+        executableUuid = params.get("executableUuid")
+        query_params = {}
+        if executableUuid:
+            endpoints_url = f"{base_url}/v1/executables/{executableUuid}"
         else:
-            raise ConnectorError(f"Failed to isolate endpoint {endpoint_id}.")
+            endpoints_url = f"{base_url}/v1/executables"
+            if params.get("pageSize"):
+                query_params.update({"pageSize": params.get("pageSize")})
+            if params.get("pageToken"):
+                query_params.update({"pageToken": params.get("pageToken")})
+
+        response = requests.get(endpoints_url, headers=headers, params=query_params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise ConnectorError('Failed to get all executables {}'.format(str(response)))
     except Exception as err:
         logger.exception('An exception occurred {}'.format(str(err)))
         raise ConnectorError('An exception occurred {}'.format(str(err)))
 
 
-def rejoin_endpoint(config, params):
+def block_unblock_executables(config, params, action):
     try:
-        base_url = config.get("base_url")
-        endpoint_id = params.get("endpoint_id")
-        token = authenticate()
-        rejoin_url = f"{base_url}/api/endpoints/{endpoint_id}/rejoin"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post(rejoin_url, headers=headers)
+        token = authenticate(config)
+        base_url = "https://eu.application-management.eset.systems" #config.get("base_url")
+        headers = {"Authorization": token, "accept": "application/json", "Content-Type": "application/json"}
+        executableUuid = params.get("executableUuid")
+        endpoints_url = f"{base_url}/v1/executables/{executableUuid}:{action}"
+        data = params.get("json_data", {})
+        response = requests.get(endpoints_url, headers=headers, data=data)
         if response.status_code == 200:
-            logger.info(f"Endpoint {endpoint_id} has rejoined the network successfully.")
-            return True
+            return response.json()
         else:
-            raise ConnectorError(f"Failed to rejoin endpoint {endpoint_id}.")
+            raise ConnectorError('Failed to get all executables {}'.format(str(response)))
     except Exception as err:
         logger.exception('An exception occurred {}'.format(str(err)))
         raise ConnectorError('An exception occurred {}'.format(str(err)))
 
 
-def list_events(config, params):
-    try:
-        base_url = config.get("base_url")
-        token = authenticate()
-        events_url = f"{base_url}/api/events"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(events_url, headers=headers)
-        if response.status_code == 200:
-            return response.json()["events"]
-        else:
-            raise ConnectorError("Failed to list events.")
-    except Exception as err:
-        logger.exception('An exception occurred {}'.format(str(err)))
-        raise ConnectorError('An exception occurred {}'.format(str(err)))
+def block_executables(config, params):
+    return block_unblock_executables(config, params, "block")
 
 
-def list_incidents(config, params):
-    try:
-        base_url = config.get("base_url")
-        token = authenticate()
-        incidents_url = f"{base_url}/api/incidents"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(incidents_url, headers=headers)
-        if response.status_code == 200:
-            return response.json()["incidents"]
-        else:
-            raise ConnectorError("Failed to list incidents.")
-    except Exception as err:
-        logger.exception('An exception occurred {}'.format(str(err)))
-        raise ConnectorError('An exception occurred {}'.format(str(err)))
-
-
-def list_files(config, params):
-    try:
-        base_url = config.get("base_url")
-        token = authenticate()
-        files_url = f"{base_url}/api/files"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(files_url, headers=headers)
-        if response.status_code == 200:
-            return response.json()["files"]
-        else:
-            raise ConnectorError("Failed to list files.")
-    except Exception as err:
-        logger.exception('An exception occurred {}'.format(str(err)))
-        raise ConnectorError('An exception occurred {}'.format(str(err)))
+def unblock_executables(config, params):
+    return block_unblock_executables(config, params, "unblock")
 
 
 operations_map = {
-    'list_endpoints': list_endpoints,
-    'isolate_endpoint': isolate_endpoint,
-    'rejoin_endpoint': rejoin_endpoint,
-    'list_files': list_files,
-    'list_incidents': list_incidents,
-    'list_events': list_events
+    'get_executables': get_executables,
+    'block_executables': block_executables,
+    'unblock_executables': unblock_executables
 }
